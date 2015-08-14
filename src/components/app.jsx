bb4e3f9532083efa97e0eb23766e88dab87bf132
Priscilla Rodriguez <priss@prisskreative.com>
2015-08-14 12:41:09 -0400

    Create channels button

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
-import { Col, Grid, Row } from 'react-bootstrap'
+import { Col, Grid, Row, Button } from 'react-bootstrap'
 
 import ChannelList from './ChannelList'
 
 class App extends Component {
+  constructor(){
+    super() 
+
+    this.state = {
+      channels:[
+       { name: '#foo' },
+       { name: '#flux' },
+       { name: '#react' },
+                ]
+    }
+  }
+
+
+
+  createChannel = () => {
+
+    const channelName = prompt("What's the channel")
+
+    const channels = this.state.channels
+
+    channels.push({ name: channelName })
+
+    this.setState({ channels })
+  }
+
+
   render() {
     return (
       <div>
         <Grid>
           <Row>
             <Col lg={4}>
-              <ChannelList channels={[
-                { name: '#foo' }
-              ]} />
+              <ChannelList channels={this.state.channels} />
+
+              <Button bsStyle ="success" 
+              onClick={this.createChannel}>
+              + Create Channel
+              </Button>
             </Col>
             <Col lg={8}>
               {this.props.children || 'Join a channel'}
